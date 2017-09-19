a730771219c536df7d3f4e91eda3867e39f8df7b
Edward Schults <edward.schults@gmail.com>
2017-09-19 10:39:01 +0200

    promoted giflist as a container

---
+++ src/containers/gif_list.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Gif from '../components/gif';
+
+class GifList extends Component {
+  renderList() {
+    return this.props.gifs.map(({ giphyId }) => {
+      return (
+        <Gif
+          key={giphyId}
+          giphyId={giphyId}
+          handleClick={() => 'TODO'}
+        />
+      );
+    });
+  };
+
+  return (
+    <div className="gif-list">
+      {this.renderList()}
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    gifs: state.gifs
+  };
+}
+
+export default GifList;
